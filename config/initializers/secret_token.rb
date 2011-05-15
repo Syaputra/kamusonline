60966f28485b50f0555a339248f8ffd36b5308dd
Charlli Syaputra <charlli.syaputra@gmail.com>
2011-05-15 20:10:43 +0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kamusonline::Application.config.secret_token = '2f7af095e51ff8684edee31702b72cc44cae9e1d62090f62212a0334675c28cfc7dddce8516d1f7c88f6da0dd07cee389d9ac69f264d751d5ef69758c75a2928'
